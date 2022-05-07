083ae74a8dba69828ddf3c5f8a922e5f0ce38c60
Shayma TRAD <64952949+shayma26@users.noreply.github.com>
2022-05-07 14:51:22 +0200

    initial

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB:
+      "mongodb+srv://mashawino:mDndPJNWCDgjX4p@cluster0.77kib.mongodb.net/soa?retryWrites=true&w=majority",
+    SECRET_KEY: "some very secret key :D",
+  };
+  
